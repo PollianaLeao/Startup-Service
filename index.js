bc0ce7b9dc59208506a77162778f38ef07ba17f6
Polliana Carvalho <polliana.carvalho@byu.edugit config --global user.name Polliana>
2023-11-22 15:43:19 -0700

    update

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const loginForm = document.getElementById('login-form');
 const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
+const passwordInput = document.getElementById('password'); 
 const message = document.getElementById('message');
 
-loginForm.addEventListener('submit', async (event) => {
+loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const username = usernameInput.value;
-    const password = passwordInput.value;
-
-    try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
-
-        const data = await response.json();
-
-        if (response.status === 200) {
-            // Authentication successful, store the token (data.token) and redirect to the user's dashboard.
-            localStorage.setItem('token', data.token);
-            window.location.href = '/dashboard.html'; // Replace with your dashboard URL.
-        } else {
-            message.textContent = 'Login failed. Please check your credentials.';
-        }
-    } catch (error) {
-        console.error('Error:', error);
+    
+    // Assuming you want to do some basic validation or use of the password in the future
+    const password = passwordInput.value; 
+    if (username && password) {
+        localStorage.setItem('username', username);
+        // Redirect to another page, like a dashboard. Change 'dashboard.html' to your desired page.
+        window.location.href = 'mainPage.html'; 
+    } else {
+        // Display an error message if the username or password is empty
+        message.textContent = 'Please enter both username and password.';
     }
 });
