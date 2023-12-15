38544c410640fa56572473cb84e321a3bb76b4c0
Polliana Carvalho <polliana.carvalho@byu.edugit config --global user.name Polliana>
2023-12-14 23:45:54 -0700

    updated

--- App.jsx
+++ App.jsx
@@ -1,14 +1,40 @@
 
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Comments } from './comments/Comments'; // Component for comments
+import { SubmitRecipe } from './submitRecipe/SubmitRecipe'; // Component for submitting recipes
+import './app.css'; // Your custom CSS
+
 function App() {
-    const [count, setCount] = useState(0);
-  
-    return (
-      <div>
-        <h1>Hello, [Your Name Here]</h1>
-        <button onClick={() => setCount(count + 10)}>
-          count is {count}
-        </button>
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <NavLink className='nav-link' to='/comments'>Comments</NavLink>
+            <NavLink className='nav-link' to='/submit-recipe'>Submit Recipe</NavLink>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path='/comments' element={<Comments />} />
+          <Route path='/submit-recipe' element={<SubmitRecipe />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className='bg-dark text-light'>
+          <div className='container-fluid'>
+            <span>Your Gluten-Free Kitchen</span>
+            <a href='https://github.com/your-github-repo'>Source</a>
+          </div>
+        </footer>
       </div>
-    );
-  }
-  
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Page Not Found</main>;
+}
+
+export default App;
